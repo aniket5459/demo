9543d2ccbcc52ec0baa3e0733e185f1712c6682e
Aniket <aniketkesari007@gmail.com>
2024-05-09 09:25:16 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ function Footer() {
         <footer className="footer">
             <div className="left">©️ Talup 2023. All rights reserved</div>
             <div className="right">
-                <a href="#">Terms & Conditions</a>
-                <a href="#">Privacy Policy</a>
+                <a href="#/">Terms & Conditions</a>
+                <a href="#/">Privacy Policy</a>
             </div>
         </footer>
     );
